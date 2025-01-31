4750823ed3dddf3badf8209045778500719cb4e1
Diego Caceres <dado90caceres@gmail.com>
2025-01-31 16:41:47 -0300

    Add Group C feature in Engagement Profile

--- src/components/EngagementChart/index.js
+++ src/components/EngagementChart/index.js
@@ -37,8 +37,10 @@ function GenericComparisonChart() {
   const [codings, setCodings] = useState([]);
   const [groupA, setGroupA] = useState(null);
   const [groupB, setGroupB] = useState(null);
+  const [groupC, setGroupC] = useState(null);
   const [errorA, setErrorA] = useState("");
   const [errorB, setErrorB] = useState("");
+  const [errorC, setErrorC] = useState("");
   const [data, setData] = useState(null);
 
   const [chiSquared, setChiSquared] = useState("");
@@ -67,29 +69,6 @@ function GenericComparisonChart() {
       fetchCodings();
     }
   }, [selectedExhibit]);
-
-  // Refresh chart when selecting values
-  useEffect(() => {
-    if (selectedExhibit && groupA && groupB) {
-      setErrorA("");
-      setErrorB("");
-
-      calculateChiSquared();
-      calculateKendallsTau();
-
-      const percentageData = calculatePercentageData();
-      console.log("percentageData", percentageData);
-
-      const dataForChart = Object.keys(percentageData).map((key) => {
-        return {
-          category: key,
-          ...percentageData[key],
-        };
-      });
-      console.log("dataForChart", dataForChart);
-      setData(dataForChart);
-    }
-  }, [selectedExhibit, groupA, groupB]);
 
   const calculateChiSquared = () => {
     let chiData = [];
@@ -131,7 +110,6 @@ function GenericComparisonChart() {
 
   const calculateKendallsTau = () => {
     let kendallsData = [];
-    debugger;
     for (const coding of codings) {
       const codingLevel = calculateCodingLevel(coding.codingBehaviors);
       const isGroupA = isCodingInGroup(coding, groupA);
@@ -161,31 +139,38 @@ function GenericComparisonChart() {
       Initiation: {
         ["Group A"]: 0,
         ["Group B"]: 0,
+        ["Group C"]: 0,
       },
       Transition: {
         ["Group A"]: 0,
         ["Group B"]: 0,
+        ["Group C"]: 0,
       },
       Breakthrough: {
         ["Group A"]: 0,
         ["Group B"]: 0,
+        ["Group C"]: 0,
       },
     };
 
     for (const coding of codings) {
       const codingLevel = calculateCodingLevel(coding.codingBehaviors);
       // Group A
-      if (isCodingInGroup(coding, groupA)) {
+      if (groupA && isCodingInGroup(coding, groupA)) {
         codingsGroupData[codingLevel]["Group A"]++;
       }
 
-      if (isCodingInGroup(coding, groupB)) {
+      if (groupB && isCodingInGroup(coding, groupB)) {
         codingsGroupData[codingLevel]["Group B"]++;
+      }
+
+      if (groupC && isCodingInGroup(coding, groupC)) {
+        codingsGroupData[codingLevel]["Group C"]++;
       }
     }
 
     // Calculate percentages within each group
-    for (const group of ["Group A", "Group B"]) {
+    for (const group of ["Group A", "Group B", "Group C"]) {
       const total = Object.values(codingsGroupData).reduce(
         (acc, val) => acc + val[group],
         0
@@ -199,7 +184,10 @@ function GenericComparisonChart() {
         } else {
           codingsGroupData[level][group] = 0;
           if (group === "Group A") setErrorA(`No matches for ${group}. `);
-          if (group === "Group B") setErrorB(`No matches for ${group}. `);
+          if (groupB && group === "Group B")
+            setErrorB(`No matches for ${group}. `);
+          if (groupC && group === "Group C")
+            setErrorC(`No matches for ${group}. `);
         }
       }
     }
@@ -221,6 +209,48 @@ function GenericComparisonChart() {
 
   const format = (v) => `${v}%`;
 
+  // Refresh chart when pressing Generate
+  const onGenerateClicked = () => {
+    // Tiene que haber al menos una Exhibit y un Grupo seleccionado
+    if (selectedExhibit && groupA) {
+      setErrorA("");
+      setErrorB("");
+      setErrorC("");
+
+      if (groupB && !groupC) {
+        calculateChiSquared();
+        calculateKendallsTau();
+      }
+
+      const percentageData = calculatePercentageData();
+      console.log("percentageData", percentageData);
+
+      const dataForChart = Object.keys(percentageData).map((key) => {
+        return {
+          category: key,
+          ...percentageData[key],
+        };
+      });
+      console.log("dataForChart", dataForChart);
+      setData(dataForChart);
+    }
+  };
+
+  const getChartBarKeys = () => {
+    const keys = [];
+    if (groupA) {
+      keys.push("Group A");
+    }
+    if (groupB) {
+      keys.push("Group B");
+    }
+    if (groupC) {
+      keys.push("Group C");
+    }
+
+    return keys;
+  };
+
   return (
     <div className="engagement-chart-container">
       <span className="page-title">Visitor Engagement Profile</span>
@@ -238,12 +268,22 @@ function GenericComparisonChart() {
         <div className="select-groups-row">
           <SelectGroupCharacteristic group="A" setGroup={setGroupA} />
           <SelectGroupCharacteristic group="B" setGroup={setGroupB} />
+          <SelectGroupCharacteristic group="C" setGroup={setGroupC} />
         </div>
 
         <div>
           {errorA}
           {errorB}
+          {errorC}
         </div>
+      </div>
+      <div className="btn-generate-container">
+        <button
+          onClick={onGenerateClicked}
+          disabled={!selectedExhibit || !groupA}
+        >
+          Generate
+        </button>
       </div>
 
       {data && (
@@ -253,14 +293,14 @@ function GenericComparisonChart() {
               <div
                 style={{
                   width: "90%",
-                  height: "600px",
+                  height: "500px",
                   backgroundColor: "white",
                   margin: "auto",
                 }}
               >
                 <ResponsiveBar
                   data={data}
-                  keys={["Group A", "Group B"]}
+                  keys={getChartBarKeys()}
                   indexBy="category"
                   margin={{ top: 20, right: 30, bottom: 150, left: 50 }}
                   groupMode="grouped"
@@ -284,13 +324,17 @@ function GenericComparisonChart() {
                   // Configure other Nivo bar chart settings here
                 />
               </div>
-              <div>
-                VEP - ChiSquaredTest
-                <div>Result: {chiSquared}</div>
-              </div>
-              <div>
-                VEP - KendallsTau B<div>Result: {kendallsData}</div>
-              </div>
+              {chiSquared && (
+                <div>
+                  VEP - ChiSquaredTest
+                  <div>Result: {chiSquared}</div>
+                </div>
+              )}
+              {kendallsData && (
+                <div>
+                  VEP - KendallsTau B<div>Result: {kendallsData}</div>
+                </div>
+              )}
             </>
           ) : (
             <p>No codings in this exhibit</p>
